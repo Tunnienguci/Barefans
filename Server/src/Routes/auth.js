e98cb1dd5ed07f94f151c868775b0976e5629faa
tunnienguci <tuanqc0712@gmail.com>
2023-10-10 01:13:33 +0700

    Update APIs route

--- Server/src/Routes/auth.js
+++ Server/src/Routes/auth.js
@@ -4,13 +4,11 @@ const router = express.Router();
 const {
 	login,
 	register,
-	getMyUser,
 	updateProfile,
 } = require("../Controllers/authController");
 
 router.post("/sign-in", login);
 router.post("/sign-up", register);
-router.get("/my-user", getMyUser);
 router.post("/sign-up/update-profile", updateProfile);
 
 module.exports = router;
